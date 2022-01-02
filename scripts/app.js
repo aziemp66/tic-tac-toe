9ef9bbfabcffa846d9eb1b5af563b8e78666ca06
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-02 23:07:20 +0700

    Validating user input

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 const playerConfigOverlay = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.getElementById("player-form");
+const errorOutput = document.getElementById("config-error");
 
 const editPlayer1Button = document.getElementById("edit-player-1");
 const editPlayer2Button = document.getElementById("edit-player-2");
--- scripts/config.js
+++ scripts/config.js
@@ -6,11 +6,18 @@ function openPlayerConfig() {
 function closePlayerConfig() {
     playerConfigOverlay.style.display = "none";
     backdropElement.style.display = "none";
+    formElement.firstElementChild.classList.remove("error");
+    errorOutput.textContent = "";
 }
 
 function savePlayerConfig(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
-    const enteredPlayerName = formData.get("playername");
-    console.log(enteredPlayerName);
+    const enteredPlayerName = formData.get("playername").trim();
+
+    if (!enteredPlayerName) {
+        event.target.firstElementChild.classList.add("error");
+        errorOutput.textContent = "Please enter a valid name!";
+        return;
+    }
 }
