275b30df86b8ead1af41e18b1d8009455f983a09
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-02 21:44:20 +0700

    Added Javascript Files

---
+++ scripts/app.js
@@ -0,0 +1,4 @@
+const editPlayer1Button = document.getElementById("edit-player-1");
+const editPlayer2Button = document.getElementById("edit-player-2");
+
+editPlayer1Button.addEventListener("click", openPlayerConfig);
