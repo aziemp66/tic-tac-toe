80b96ec4ef1f4ec688c958a43bcc554e7235a091
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-02 21:58:46 +0700

    Confiuring Overlays

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
+const playerConfigOverlay = document.getElementById("config-overlay");
+const backdrop = document.getElementById("backdrop");
+
 const editPlayer1Button = document.getElementById("edit-player-1");
 const editPlayer2Button = document.getElementById("edit-player-2");
+const cancelConfigButton = document.getElementById("cancel-config-button");
 
 editPlayer1Button.addEventListener("click", openPlayerConfig);
+editPlayer2Button.addEventListener("click", openPlayerConfig);
+
+cancelConfigButton.addEventListener("click", closePlayerConfig);
+backdrop.addEventListener("click", closePlayerConfig);
--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,9 @@
-function openPlayerConfig() {}
+function openPlayerConfig() {
+    playerConfigOverlay.style.display = "block";
+    backdrop.style.display = "block";
+}
+
+function closePlayerConfig() {
+    playerConfigOverlay.style.display = "none";
+    backdrop.style.display = "none";
+}
